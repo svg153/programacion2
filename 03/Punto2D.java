fbde16710d1ac4d02876dfcb83e622f044feecd0
Ángel Herranz <aherranz@fi.upm.es>
2018-02-14 09:19:34 +0100

    Uso de algunos modificadores.

--- 03/Punto2D.java
+++ 03/Punto2D.java
@@ -1,41 +1,56 @@
 class Punto2D {
   // Atributos: coordenadas polares
+  // Visibilidad: public, private, protected
   private double r;
   private double a;
+
+  // Variable global compartida por todas
+  // las instancias y accesible desde el
+  // código cliente mediante Punto2D.global
+  // Nota: no se puede cambiar la referencia
+  // pero só el contenido.
+  final static public Punto2D global =
+    new Punto2D();
+
   // Constructores
-  Punto2D() {
+
+  // Crea el punto (0, 0)
+  public Punto2D() {
     r = 0;
   }
-  Punto2D (double x,
-           double y) {
+
+  // Crea el punto (x,y) (cartesianas!!!)
+  public Punto2D (double x,
+                  double y) {
     this.setXY(x,y);
   }
-  // Métodos "normales" del objeto
-  double distanciaAl0() {
+
+  // Métodos "públicos" del objeto
+  public double distanciaAl0() {
     return r;
   }
 
-  void setX(double x) {
+  public void setX(double x) {
     double y = this.getY();
     setXY(x, y);
   }
   
-  double getX() {
+  public double getX() {
     return r * Math.cos(a);
   }
   
-  void setY(double y) {
+  public void setY(double y) {
     double x = this.getX();
     setXY(x, y);
   }
   
-  double getY() {
+  public double getY() {
     return r * Math.sin(a);
   }
 
   // Métodos auxiliares
-  void setXY(double x,
-             double y) {
+  private void setXY(double x,
+                     double y) {
     this.r = Math.sqrt(x*x + y*y);
     if (x == 0) {
       if (y < 0) {
