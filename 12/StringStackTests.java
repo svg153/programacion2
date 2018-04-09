367fbae8186952061d559b4ae6185afc2290c3c9
Ángel Herranz <aherranz@fi.upm.es>
2018-04-09 13:13:42 +0200

    Aserciones con texto + nuevo tests de push y top.

--- 12/StringStackTests.java
+++ 12/StringStackTests.java
@@ -7,13 +7,32 @@ public class StringStackTests {
   public void lasPilasNuevasEstánVacías() {
     StringStack s;
     s = new StringStack(10);
-    assertTrue(s.isEmpty());
+    assertTrue("Las pilas nuevas tienen que estar vacías",
+               s.isEmpty());
   }
 
   @Test
   public void lasPilasNuevasNoEstánLlenas() {
     StringStack s;
     s = new StringStack(10);
-    assertFalse(s.isEmpty());
+    assertFalse("Una pila nueva no debe estar llena",
+                s.isFull());
+  }
+
+  @Test
+  public void pushYtop() {
+    String palabra = "cualquier palabra";
+    StringStack s;
+    s = new StringStack(10);
+    s.push(palabra);
+    assertFalse("Una pila tras un push no puede estar vacía",
+                s.isEmpty());
+    assertFalse("Un sólo push no llena una pila",
+                s.isFull());
+    assertTrue("La cima de la pila es lo último que hayas empujado",
+               palabra.equals(s.top()));
+    s.pop();
+    assertTrue("Un push y un pop dan una pila vacía",
+               s.isEmpty());
   }
 }
