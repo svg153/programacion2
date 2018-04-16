a3ade67f43be97dcc83384707a9f98c0c29eb23e
Ángel Herranz <aherranz@fi.upm.es>
2018-04-16 11:23:56 +0200

    Sesión 14: puesta en marcha.

---
+++ 14/UsandoArrayList.java
@@ -0,0 +1,11 @@
+import list.ArrayList;
+
+public class UsandoArrayList {
+
+  public static void main(String[] args) {
+    String a[];
+    String s = "Hola me llamo pepe";
+    a = s.split(" ");
+    System.out.println("Programa principal" + a.length);
+   }
+}
