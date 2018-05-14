bc79fbd0c5e157b0113784eec69f8f12f1d7fa86
Ángel Herranz <aherranz@fi.upm.es>
2018-05-14 12:22:03 +0200

    Añadido un constructor para facilitar la creación de instancias SimpleNode.

--- 20/src/g1mb/eds/SimpleNode.java
+++ 20/src/g1mb/eds/SimpleNode.java
@@ -1,9 +1,14 @@
 package g1mb.eds;
 
 /**
- * Nodos para cadenas enlazadas simples y circulares simples.
+ * Nodos para cadenas enlazadas simples
+ * y circulares simples.
  */
 public class SimpleNode<E> {
   public E data = null;
   public SimpleNode<E> next = null;
+  public SimpleNode<E>(E x,
+                       SimpleNode<E> y) {
+    this.data = x;
+    this.next = y;
 }
